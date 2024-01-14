ad5ced17e5b1a9d3e73e29203180e814708ff9b9
nini0919 <1109022775@qq.com>
2024-01-14 19:13:58 +0800

    1

--- run.sh
+++ run.sh
@@ -1,16 +1,6 @@
 
-
-# if exist InternLM then remove it
-if [ -d InternLM ]; then
-    rm -rf InternLM
-fi
-
-
-openxlab model download --model-repo 'fiv/DreamBinaryAssistant_Merge' --output=model
-
 git clone https://github.com/InternLM/InternLM.git
 
-cp app.py InternLM/
+cp InternLM/
 
-
-streamlit run InternLM/app.py --server.address=0.0.0.0 --server.port 7860
+streamlit run app.py --server.address=0.0.0.0 --server.port 7860
